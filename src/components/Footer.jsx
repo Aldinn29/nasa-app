44992b78e84317753b5191603433bdc1de68ff22
Aldinn29 <aldinbisanovic1rt@gmail.com>
2025-03-10 11:34:55 +0100

    Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer(props) {
     const { data, toggleSideBar } = props
   return (
-    <div className='footer text-white position-fixed bottom-0 w-100 p-3 d-flex justify-content-between align-items-center p-5 z-1'>
+    <div className='footer text-white position-fixed bottom-0 w-100 p-3 d-flex justify-content-between align-items-center p-5 p-md-2 z-1'>
       <div className='d-flex flex-column gap-2'>
         <h1>{data.title}</h1>
         <h1>APOD PROJECT</h1>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,7 @@ export default function SideBar(props) {
     const { data, toggleSideBar } = props
 
   return (
-    <div className='sidebar vh-100 w-25 bg-dark text-white p-3 top-0 end-0 position-fixed d-flex flex-column justify-content-between z-2'>
+    <div className='sidebar vh-100 w-25 w-md-50 bg-dark text-white p-3 top-0 end-0 position-fixed d-flex flex-column justify-content-between z-2'>
       <div>
         <h1>{data.title}</h1>
         <h3 className='my-3'>{data.date}</h3>
